87f228df9074fb3d61acd0d283dd874d7d3315bc
Rez <reza.amini.majd1379@gmail.com>
2020-01-29 15:59:05 +0330

    finish

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 //
 // Created by rez on 26.01.20.
 //
-#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define CAR_WIDTH 2
 #define CAR_LENGTH 3
@@ -11,62 +13,131 @@
 #define SPEED_HARD_MODE 2
 #define COUNT_OF_OBSTACLE 5
 #define COUNT_OF_POINT_AN_OBJECT 6
+
 struct Car {
     int x;
     int y;
-    int velocity;
-} car;
+};
 
 
 struct Obstacle {
     int x;
     int y;
+    int isExist;
 };
 
-struct Obstacle obstacles[COUNT_OF_OBSTACLE];
-int gameMap[8][16];
+int gameMap[16][8];
+int loopCounter = 0;
+int last_counter = 0;
 int gameSpeed = SPEED_EASY_MODE;
-bool end = false;
+int end = 0;
 
-void setup() {
-    // put your setup code here, to run once:
+
+void init(struct Obstacle obstacles[]);
+
+void drawLED(int x, int y);
+
+void createRandomObstacles(struct Obstacle *obstacles);
+
+void moveRight(struct Car *car);
+
+void moveLeft(struct Car *car);
+
+void checkForCollision(struct Car *car, struct Obstacle *obstacles);
+
+void makeMapReady(struct Car *car, struct Obstacle obstacles[]);
+
+void showMap();
+
+void changeDifficulty();
+
+void drawObstaclesOnLED(struct Obstacle obstacles[]);
+
+//change to setup and loop
+int main() {
+    //setup
+    struct Obstacle obstacles[COUNT_OF_OBSTACLE];
+    struct Car car = {};
+    init(obstacles);
+    car.x = 13;
+    car.y = 3;
+    //end_setup
+    // loop
+    while (!end) {
+        createRandomObstacles(obstacles);
+        makeMapReady(&car, obstacles);
+        showMap();
+        int rol;
+        scanf("%d", &rol);
+        if (rol == 1) {
+            moveLeft(&car);
+        } else if (rol == 2) {
+            moveRight(&car);
+        } else if (rol == 3) {
+            changeDifficulty();
+        } else if (rol == 4) {
+            end = 1;
+        }
+        for (int i = 0; i < COUNT_OF_OBSTACLE; ++i) {
+            if (obstacles[i].isExist) {
+                obstacles[i].x += gameSpeed;
+                if (obstacles[i].x > 15) {
+                    obstacles[i].isExist = 0;
+                }
+            }
+        }
+        checkForCollision(&car, obstacles);
+        loopCounter += gameSpeed;
+    }
+    //end_loop
 
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
-
+void drawLED(int x, int y) {
+    gameMap[x][y] = 3;
 }
 
-
-void createRandomObstacles() {
-    // TODO ShAli
+void createRandomObstacles(struct Obstacle *obstacles) {
+    if (loopCounter - 6 > last_counter) {
+        last_counter = loopCounter;
+        srand(time(0));
+        for (int i = 0; i < 5; ++i) {
+            int y = (rand() % 3) * 2 + 1;
+            if (obstacles[i].isExist == 0) {
+                struct Obstacle obstacle = {0, y, 1};
+                obstacles[i] = obstacle;
+                break;
+            }
+        }
+    }
 }
 
-void moveRight() {
-    car.y++;
+void moveRight(struct Car *car) {
+    if ((*car).y < 5)
+        (*car).y += 2;
 }
 
-void moveLeft() {
-    car.y--;
+void moveLeft(struct Car *car) {
+    if ((*car).y > 2)
+        (*car).y -= 2;
 }
 
-
-void checkForCollision() {
-    int car_y = car.y;
+void checkForCollision(struct Car *car, struct Obstacle *obstacles) {
+    int car_y = (*car).y;
     for (int i = 0; i < COUNT_OF_OBSTACLE; ++i) {
-        int obstacle_x = obstacles[i].x;
-        int obstacle_y = obstacles[i].y;
-        if (obstacle_x < CAR_LENGTH + OBSTACLE_LENGTH) {
-            if (obstacle_y <= car_y + 1 && obstacle_y >= car_y - 1) {
-                end = true;
-                return;
+        if (obstacles[i].isExist) {
+            int obstacle_x = obstacles[i].x;
+            int obstacle_y = obstacles[i].y;
+            if (obstacle_x > 10) {
+                if (obstacle_y <= car_y + 1 && obstacle_y >= car_y - 1) {
+                    end = 1;
+                    return;
+                }
             }
         }
     }
 
 }
-
 
 void changeDifficulty() {
     if (gameSpeed == SPEED_EASY_MODE) {
@@ -76,36 +147,66 @@ void changeDifficulty() {
     }
 }
 
-void drawLED(int x, int y) {
-    //TODO Rez
-}
 void getAllPointsFromFirstPoint(int x, int y, int allPoints[][2]) {
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 2; ++j) {
-            allPoints[i + j][0] = x + i;
-            allPoints[i + j][1] = y + j;
+            allPoints[i * 2 + j][0] = x + i;
+            allPoints[i * 2 + j][1] = y + j;
         }
     }
 }
 
-void drawObstaclesOnLED() {
+void drawObstaclesOnLED(struct Obstacle obstacles[]) {
+
     for (int i = 0; i < COUNT_OF_OBSTACLE; ++i) {
         int points[COUNT_OF_POINT_AN_OBJECT][2];
         getAllPointsFromFirstPoint(obstacles[i].x, obstacles[i].y, points);
         for (int j = 0; j < COUNT_OF_POINT_AN_OBJECT; ++j) {
-            drawLED(points[j][0], points[j][1]);
+            if (obstacles[i].isExist && points[j][0] < 16) {
+                drawLED(points[j][0], points[j][1]);
+            }
         }
     }
 }
 
-void drawCarOnLED() {
-    int points[COUNT_OF_POINT_AN_OBJECT][2];
-    getAllPointsFromFirstPoint(car.x, car.y, points);
-    for (int j = 0; j < COUNT_OF_POINT_AN_OBJECT; ++j) {
-        drawLED(points[j][0], points[j][1]);
+void makeMapReady(struct Car *car, struct Obstacle obstacles[]) {
+    for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if (j == 0 || j == 7) {
+                gameMap[i][j] = 1;
+            } else {
+                gameMap[i][j] = 0;
+            }
+        }
     }
+    int carPoints[6][2];
+    getAllPointsFromFirstPoint((*car).x, (*car).y, carPoints);
+    for (int k = 0; k < 6; ++k) {
+        gameMap[carPoints[k][0]][carPoints[k][1]] = 2;
+    }
+    drawObstaclesOnLED(obstacles);
 }
 
+// for test
+void showMap() {
+    for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            printf("%d ", gameMap[i][j]);
+        }
+        printf("\n");
+    }
 
+}
 
-
+void init(struct Obstacle obstacles[]) {
+    struct Obstacle obstacle = {0, 0, 0};
+    struct Obstacle obstacle1 = {0, 0, 0};
+    struct Obstacle obstacle2 = {0, 0, 0};
+    struct Obstacle obstacle3 = {0, 0, 0};
+    struct Obstacle obstacle4 = {0, 0, 0};
+    obstacles[0] = obstacle;
+    obstacles[1] = obstacle1;
+    obstacles[2] = obstacle2;
+    obstacles[3] = obstacle3;
+    obstacles[4] = obstacle4;
+}
